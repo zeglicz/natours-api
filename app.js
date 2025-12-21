db1c03df26af26c5290a3e0c62f2a05c78346d36
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-21 14:07:48 +0100

    feat: handle GET api/v1/tours request

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // Send HTML - default 200, if we don't specify exact status code
-  // res.status(200).send('Hello from the server side!');
+// app.get('/', (req, res) => {
+//   // Send HTML - default 200, if we don't specify exact status code
+//   // res.status(200).send('Hello from the server side!');
+//   res.status(200).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours',
+//   });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours',
+    status: 'success',
+    resaults: tours.length,
+    data: { tours },
+    // data: { tours: tours },
   });
 });
 
