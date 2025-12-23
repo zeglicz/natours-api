015b81fa9aa436884c07a11e7c77acdcd18d0e7e
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-23 23:15:47 +0100

    feat: add /users routes and create callback placeholders (500)

--- app.js
+++ app.js
@@ -138,6 +138,41 @@ const deleteTour = (req, res) => {
 //   res.status(200).json({ status: 'success' });
 // });
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
 ////////////////////////
 // 3. ROUTES
 ////////////////////////
@@ -161,6 +196,14 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 ////////////////////////
 // 4, START SERVER
