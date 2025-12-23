c59adeb7ba5c5bdd7ce10ec2135ae0c9d4a295b4
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-24 00:00:01 +0100

    refactor: move api callback functions/handlers to /controllers

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,96 +1,17 @@
-const fs = require('fs');
 const express = require('express');
+
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-////////////////////////
-
-const getAllTours = (req, res) => {
-  console.log(req.reqestTime);
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    requestedAt: req.reqestTime,
-    data: { tours },
-    // data: { tours: tours },
-  });
-};
-
-const getTour = (req, res) => {
-  // console.log(req.params);
-  const id = Number(req.params.id);
-  const tour = tours.find((t) => t.id === id);
-
-  if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      messsage: 'Invalid ID',
-    });
-  // console.log(tour);
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
-
-  // console.log(req.body);
-  // We always need to send back something in order to finish the so-called request/response cycle
-  // res.send('Done');
-};
-
-const updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here…>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-////////////////////////
-
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
