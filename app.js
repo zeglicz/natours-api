2544e90010bae87b87aa9d44fb40830e40e01e5e
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-23 23:30:46 +0100

    refactor: extract app.routes into express.Router middleware

--- app.js
+++ app.js
@@ -183,27 +183,39 @@ const deleteUser = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+// app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
-// routes from up end req-res cycle so this doesn't show
-app.use((req, res, next) => {
-  console.log('Hello from the middleware between routes');
-  next();
-});
+// // routes from up end req-res cycle so this doesn't show
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware between routes');
+//   next();
+// });
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+// app
+//   .route('/api/v1/tours/:id')
+//   .get(getTour)
+//   .patch(updateTour)
+//   .delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+// app.route('/api/v1/users').get(getAllUsers).post(createUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+// app
+//   .route('/api/v1/users/:id')
+//   .get(getUser)
+//   .patch(updateUser)
+//   .delete(deleteUser);
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 ////////////////////////
 // 4, START SERVER
