2ec1ab98abc4d4e48da3e28ac2d2230977825c13
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-24 00:08:04 +0100

    refactor: create server.js (current main file); move nodeon to devDependencies

--- app.js
+++ app.js
@@ -226,7 +226,9 @@ app.use('/api/v1/users', userRouter);
 // 4, START SERVER
 ////////////////////////
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}…`);
-});
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}…`);
+// });
+
+module.exports = app;
