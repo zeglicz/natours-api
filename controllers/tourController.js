8d8a69f22157979236ce43c727e3e07d765bcef0
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-25 13:13:49 +0100

    feat: add param middleware which checks is ID is valid

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,21 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, value) => {
+  console.log(value);
+
+  const id = Number(value);
+  const tour = tours.find((t) => t.id === id);
+
+  if (!tour)
+    // return is important, it's break the response, without return function will send status and then continoue with next()
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.reqestTime);
   res.status(200).json({
@@ -17,14 +32,14 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   // console.log(req.params);
-  const id = Number(req.params.id);
-  const tour = tours.find((t) => t.id === id);
+  // const id = Number(req.params.id);
+  // const tour = tours.find((t) => t.id === id);
 
-  if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      messsage: 'Invalid ID',
-    });
+  // if (!tour)
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     messsage: 'Invalid ID',
+  //   });
   // console.log(tour);
 
   res.status(200).json({
@@ -56,11 +71,11 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
+  // if (Number(req.params.id) > tours.length)
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
 
   res.status(200).json({
     status: 'success',
@@ -71,11 +86,11 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
+  // if (Number(req.params.id) > tours.length)
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
 
   res.status(204).json({
     status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,16 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+// PARAM MIDDLEWARE for exact path API (not for all) '/api/v1/tours/:id'
+// Each router is like mini sub-application, on for each resource
+// router.param('id', (req, res, next, value) => {
+//   // var value hold the of the (in this ex.) id parameter
+//   console.log(value);
+//   next();
+// });
+
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
