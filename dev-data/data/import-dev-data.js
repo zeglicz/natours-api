6f4d1d8402f2cab75610414aaf9aa7d223a26dcf
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-27 21:57:49 +0100

    chore: create file to delete/import data into mongoDB from json

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,44 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+const DB = process.env.MONGO_DB.replace(
+  '<PASSWORD>',
+  process.env.MONGO_DB_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') importData();
+if (process.argv[2] === '--delete') deleteData();
