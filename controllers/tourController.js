d0631dd61b47d93687993eb963af5110dacdc3b3
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-28 17:36:28 +0100

    feat: add fields limit to GET /api/v1/tours?fields=name,price

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3. Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
 
