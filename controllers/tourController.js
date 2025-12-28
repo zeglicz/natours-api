f36a6b5ee2c3eecac89b66a93a930174d17f137a
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-12-28 16:24:17 +0100

    feat: add filtering (query string) to GET /api/v1/tours?...

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,30 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // console.log(req.query, queryObj);
+    const query = await Tour.find(queryObj);
+
+    // const query = await Tour.find({
+    // duration: 5,
+    // difficulty: 'easy',
+    // });
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
