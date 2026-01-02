7d754519f57a70e86d364298233df3d1ddc3162d
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-01-02 21:37:17 +0100

    feat: add custom validator to check if password is equal to passwordConfirm; add middleware pre 'save' to encrypt password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE (User.save/create)
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  // Don't need next() - mongoose will waits for the promise to resolve
 });
 
 const User = mongoose.model('User', userSchema);
