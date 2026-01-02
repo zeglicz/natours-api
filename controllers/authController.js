7c6a0f64a3dea52b0ccdb79f2519b5c60b59bddd
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-01-02 21:54:51 +0100

    fix: restrict user creation to explicit whitelisted fields

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,14 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // With this solution some can add to body permision like 'admin' and get extra access to the app
+  //   const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
