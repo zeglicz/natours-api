9f8494b086634f8d2cb3737b29cf5fe093fde68a
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2026-01-05 15:02:54 +0100

    feat: add helmet and add docs to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,23 +10,37 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1. GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+// Limit request from same API
 const limiter = rateLimit({
   max: 100,
   windowMS: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
+
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.reqestTime = new Date().toISOString();
   // console.log(req.headers);
   next();
 });
+
+// 2. ROUTES
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
@@ -43,6 +58,8 @@ app.all('*', (req, res, next) => {
 
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// 3. GLOBAL ERROR HANDLER
 
 // When we use 4 argument express recognize this as a error handleing middleware and only call it if it's error
 app.use(globalErrorHandler);
